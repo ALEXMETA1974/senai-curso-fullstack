b8249179d85904960bfc8644042e129988c1449c
ALEXMETA1974 <alexandreaaf@hotmail.com>
2022-03-14 17:14:46 -0300

    Adiciona Encontro Remoto UC8

---
+++ script.js
@@ -0,0 +1,30 @@
+//Criar um contador de 0 a 10
+/*console.log("Contando de 0 a 10")
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log(6)
+console.log(7)
+console.log(8)
+console.log(9)
+console.log(10)*/
+
+for (let contador = 0;  contador <=10; contador++) {
+
+//Códido que será repetido
+console.log(contador);
+// Localizar o número 8
+if (contador == 8) {
+    //resultado da condição verdadeiro
+    console.log("O numero foi localizao")
+} else
+    //resultado da condição falso
+    console.log("O numero não foi localizao")
+
+}
+
+
+
+
